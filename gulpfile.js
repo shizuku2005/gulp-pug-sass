ace230a8a1dcd3dc4256c07a92198fabdbce35d7
Robert <kazuma.takeda@claves.co.jp>
2018-03-29 12:00:15 +0900

    gulpの監視タスクを修正

--- gulpfile.js
+++ gulpfile.js
@@ -239,7 +239,7 @@ gulp.task('browser-sync', function() {
 // gulp実行時に発火させるタスクと、ファイルの監視の設定
 gulp.task('default', ['html','styles','javascript','imagemin','copy-other','browser-sync'], function () {
   gulp.watch('./src/**/*.pug', ['html'])
-  gulp.watch('./src/scss/**/*.+(scss|sass)', ['styles'])
+  gulp.watch('./src/sass/**/*.+(scss|sass)', ['styles'])
   gulp.watch('./src/js/**/*.js', ['javascript'])
   gulp.watch('./src/img/**/*.+(jpg|jpeg|png|gif|svg|ico)', ['imagemin'])
   gulp.watch('./src/other/**/*', ['copy-other'])
